cfeaad1c7b7ecb2416408e0272048cc115a0ca4f
Hapachino <erickwbrownfield@gmail.com>
2019-03-04 23:29:37 -0800

    signup form

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const SignUp = () => {
+import { SIGN_UP } from '../../constants/routes';
+
+const SignUpPage = () => {
   return (
     <div>
-
+      <h1>Sign Up</h1>
+      <SignUpForm />
     </div>
   )
 }
 
-export default SignUp;
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  password: '',
+  passwordConfirm: '',
+  error: null,
+};
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = e => {
+
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  renderInput = (name, placeholder, type) => {
+    const { onChange } = this;
+
+    return (
+      <input 
+        type={type || 'text'} 
+        name={Object.keys({ name })[0]}
+        value={name}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    );
+  }
+
+  render() {
+    const { onChange, onSubmit, renderInput } = this;
+    const {
+      username,
+      email,
+      password,
+      passwordConfirm,
+      error,
+    } = this.state;
+
+    return(
+      <form onSubmit={onSubmit}>
+        {renderInput(username, 'Full Name')}
+        {renderInput(email, 'Email Address', 'email')}
+        {renderInput(password, 'Password', 'password')}
+        {renderInput(passwordConfirm, 'Password Confirm', 'password')}
+        <button type="submit">Sign Up</button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={SIGN_UP}>Sign UP</Link>
+  </p>
+);
+
+export default SignUpPage;
+export { SignUpForm, SignUpLink };
