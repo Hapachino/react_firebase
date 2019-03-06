5f7828ed85f557967030049d6bab45594211e563
Hapachino <erickwbrownfield@gmail.com>
2019-03-05 20:05:13 -0800

    PasswordChangeForm component

--- src/components/PasswordChange/index.js
+++ src/components/PasswordChange/index.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PasswordChange = () => {
-  return (
-    <div>
+import Input from '../Input';
+import { withFirebase } from '../Firebase';
 
-    </div>
-  )
+const INITIAL_STATE = {
+  password: '',
+  passwordConfirm: '',
+  error: null,
+};
+
+class PasswordChangeForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = async e => {
+    const { props: { firebase }, state: { password } } = this;
+
+    e.preventDefault();
+
+    try {
+      await firebase.doPasswordUpdate(password);
+
+      this.setState({ ...INITIAL_STATE });
+    } catch(error) {
+      this.setState({ error });
+    }
+  };
+
+  render() {
+    const { onChange, onSubmit, state: { password, passwordConfirm, error } } = this;
+    const isInvalid = !password || password !== passwordConfirm;
+
+    return (
+      <form onSubmit={onSubmit}>
+        {Input(onChange, password, 'password', 'New Password', 'password')}
+        {Input(onChange, passwordConfirm, 'password', 'Confirm New Password', 'password')}
+
+        <button disabled={isInvalid}>
+          Reset Password
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
 }
 
-export default PasswordChange;
+export default withFirebase(PasswordChangeForm);
