fa1052a793e2bfd7460d34999b8b4e0f147dce24
Hapachino <erickwbrownfield@gmail.com>
2019-03-05 20:22:17 -0800

    withAuthorization component

---
+++ src/components/Session/withAuthorization.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { SIGN_IN } from '../../constants/routes'
+import { withFirebase } from '../Firebase';
+
+const withAuthorization = condition => WrappedComponent => {
+  class WithAuthorization extends Component {
+    componentDidMount() {
+      const { firebase, history } = this.props;
+
+      this.listener = firebase.auth.onAuthStateChanged(
+        authUser => {
+          if (!condition(authUser)) {
+            history.push(SIGN_IN);
+          }
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      this.listener();
+    } 
+
+    render() {
+      return <Component {...this.props} />
+    }
+  }
+
+  return compose(
+    withFirebase,
+    withRouter
+  )(WithAuthorization);
+}
+
+export default withAuthorization;
