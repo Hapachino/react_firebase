6bdfad5edff098276207f2b054822904c763840a
Hapachino <erickwbrownfield@gmail.com>
2019-03-05 20:25:24 -0800

    HomePage component using withAuthorization

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -24,7 +24,7 @@ const withAuthorization = condition => WrappedComponent => {
     } 
 
     render() {
-      return <Component {...this.props} />
+      return <WrappedComponent {...this.props} />
     }
   }
 
