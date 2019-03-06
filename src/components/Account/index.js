de6da5b3b85b67e94ef9697ada2667b74dc0dde4
Hapachino <erickwbrownfield@gmail.com>
2019-03-05 20:42:42 -0800

    route protection - broard-grained authorization

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
-import { withAuthorization } from '../Session';
+import { AuthUserContext, withAuthorization } from '../Session';
 
-const AccountPage = () => {
-  return (
-    <div>
-      <h1>Account Page</h1>
-      <PasswordForgetForm />
-      <PasswordChangeForm />
-    </div>
-  )
-}
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
+);
 
 const condition = authUser => !!authUser;
 
--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
 import { SIGN_IN } from '../../constants/routes'
+import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 
 const withAuthorization = condition => WrappedComponent => {
@@ -21,10 +22,16 @@ const withAuthorization = condition => WrappedComponent => {
 
     componentWillUnmount() {
       this.listener();
-    } 
+    }
 
     render() {
-      return <WrappedComponent {...this.props} />
+      return (
+        <AuthUserContext.Consumer>
+          {authUser => 
+            condition(authUser) ? <WrappedComponent {...this.props} /> : null
+          }
+        </AuthUserContext.Consumer>
+      );
     }
   }
 
