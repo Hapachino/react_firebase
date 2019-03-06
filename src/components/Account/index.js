77dd3e2715ab966e1fc02da6d56add59027ba598
Hapachino <erickwbrownfield@gmail.com>
2019-03-05 20:07:32 -0800

    AccountPage component

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Account = () => {
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+
+const AccountPage = () => {
   return (
     <div>
-
+      <h1>Account Page</h1>
+      <PasswordForgetForm />
+      <PasswordChangeForm />
     </div>
   )
 }
 
-export default Account;
+export default AccountPage;
